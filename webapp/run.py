688e1f9347ff32a4f9096893170c49f73dd5947b
moosd <souramoo@gmx.com>
2015-07-24 17:01:35 +0100

    Yay for smooth map code!!! And a tile server!

--- webapp/run.py
+++ webapp/run.py
@@ -2,11 +2,23 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
+from PIL import Image
+import StringIO
+
 def redir(request):
     return Response('<meta http-equiv="refresh" content="0;URL=/ui/main.html">')
 
 def tile(request):
-    return Response(request.matchdict['x'] + "," + request.matchdict['y'] + "," + request.matchdict['z'])
+    img_filename = "ui/stage.jpg"
+    im = Image.open(img_filename)
+    x=int(request.matchdict['x']) * 256
+    y=int(request.matchdict['y']) * 256
+    output = StringIO.StringIO()
+    im.crop((x, y, x+256, y+256)).save(output, format='png')
+    contents = output.getvalue()
+    output.close()
+    return Response(contents, content_type="image/png")
+
 
 if __name__ == '__main__':
     config = Configurator()
