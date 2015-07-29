e2a5dcad99a9d521faae94e7ed1efe390c50fa85
sourtin <wje25@cam.ac.uk>
2015-07-29 10:08:07 +0100

    Stubs for virtual shapeoko

---
+++ virtual.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from interface.hw import Stage, Head, Camera, Status
+
+class ΧΥ(Stage):
+    def _initialise(self):
+        pass
+
+    
+
+class Σ(Camera):
+    def __init__(self):
+        pass
+
+class Μ(Camera):
+    pass
+
+class Ρen(Head):
+    pass
+
+
