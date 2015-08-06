daddc4a60a730727ad969541a7348d9723d7ee48
sourtin <igem@sourt.in>
2015-08-06 16:32:52 +0100

    Refactoring

--- var/tests/circles.py
+++ var/tests/circles.py
@@ -2,27 +2,29 @@
 from hw.shapeoko import Shapeoko, Axes
 from interface.vector import Vector
 from math import pi
-from time import sleep
+import time
 import glob
 
 if __name__ == '__main__':
     com = glob.glob('/dev/ttyACM*')[0]
+    print(com)
     stage = Shapeoko(com)
+    time.sleep(2)
     stage.home([Axes.X, Axes.Y, Axes.Z])
 
     origin = Vector(100,100)
     radius = 80
     d2r = lambda deg: deg * pi / 180
-    move = lambda v: stage.move([v.x(), v.y(), z])
+    move = lambda v: stage.move([v.x(), v.y(), None])
+
+    time.sleep(15)
     
     move(origin)
     for deg in range(0, 360, 15):
         point = origin + Vector.from_polar(radius, d2r(deg))
         move(point)
-        time.sleep(0.4)
+        time.sleep(4)
 
     stage.close()
-
-
-
+    time.sleep(60)
 
