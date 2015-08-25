193bde6fa1d07a0e53e292302b09da2c870ba3e8
sourtin <igem@sourt.in>
2015-08-25 18:22:38 +0100

    Necessary

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install python3.4 python3-numpy build-essential python3-pil python3-flask fail2ban hostapd python3-pip
+sudo apt-get -y install python3.4 python3-numpy build-essential python3-pil python3-flask fail2ban hostapd python3-pip vim
 
 # Copy config
 sudo cp raspi_conf/rc.local /etc/rc.local
