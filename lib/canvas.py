fce4c1a10a6ecdc9f3fac55382a3158fd58084e2
sourtin <william.earley@gmail.com>
2015-09-01 17:26:55 +0100

    More stitching tests and the beginning of a general implementation

--- lib/canvas.py
+++ lib/canvas.py
@@ -10,7 +10,7 @@ import subprocess
 
 import cv2
 import numpy as np
-import PIL
+import PIL.Image
 
 class Image(object):
     """Image wrapper with helper functions for conversion between cv/numpy and PIL formats;
