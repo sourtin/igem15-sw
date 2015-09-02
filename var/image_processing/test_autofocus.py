1d1120e64db9343e9d291745054188a181c8b52b
ra428 <ra428@cam.ac.uk>
2015-09-02 10:51:51 +0000

    More testing autofocus

--- var/image_processing/test_autofocus.py
+++ var/image_processing/test_autofocus.py
@@ -3,12 +3,15 @@ import picamera
 import picamera.array
 import cv2
 import time
+import numpy as np
 
-with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(0.1)
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.capture(stream, format = 'gray')
-        image = stream.array
+for i in range(9999999):
+	with picamera.PiCamera() as camera:
+	    camera.start_preview()
+	    time.sleep(0.1)
+	    with picamera.array.PiRGBArray(camera) as stream:
+	        camera.capture(stream, format = 'bgr')
+	        image = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
 
-print(image.dtype, image.shape)
+	print(np.var(image))
+	#time.sleep(2)
