b9b9ff1c00939b339d031f64aec346e4107600d7
moosd <souramoo@gmx.com>
2015-09-10 18:23:20 +0100

    Preliminary z-stacking and timelapsing support (backend)

---
+++ gui/webshell/timelapser.py
@@ -0,0 +1,32 @@
+import threading
+import time
+
+import sys
+sys.path.append("/home/pi/igem15-sw/")
+
+from gui.webshell.mjpgstreamer import MjpgStreamer
+
+class Timelapser(threading.Thread):
+    def __init__(self, tl):
+        super(StoppableThread, self).__init__()
+        self._stop = threading.Event()
+        self._times = tl[2]+1
+        self._delay = tl[1]
+        self._garcon = 0
+        self._user = tl[0]
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def run(self):
+        while not self.stopped() and self._times > 0:
+            if self._garcon == 0
+                self._times -= 1
+                self._garcon = self._delay
+                MjpgStreamer.captureImg(self._user)
+            self._garcon -= 1
+            time.delay(1)
+        self.stop()
