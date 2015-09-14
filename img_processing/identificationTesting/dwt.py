b0f2181718c0d93689fe758dfa25aa51b822b409
ra428 <rajiv5ashok@gmail.com>
2015-09-14 14:27:41 +0100

    Split the code into two scripts
    One of them purely does dwt, the other has gradient descent and and code
    to control microscope

--- img_processing/identificationTesting/dwt.py
+++ img_processing/identificationTesting/dwt.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import cv2
-import time
 
 def dwt(X, h1 = np.array([-1, 2, 6, 2, -1])/8 , h2 = np.array([-1, 2 -1])/4):
     """ 
@@ -116,134 +115,18 @@ def focus_score(X):
         i += 1
         score += np.var(i)
     return score
-
-def golden_section(a,d):
-    """Given an interval, returns the next two intervals using the golden ratio
-    The interval is arranged as [a, b, c, d]
-    """
-    golden = (1 + 5 ** 0.5) / 2     # Golden ratio
-
-    # New interval point to test
-    delta_x = d * (1 - 1/golden)
-    b = a + delta_x
-    #c = d - delta_x
-    
-    return b
-
-def golden_section_interval_reduction(interval, f, finterval,  tolerance = 50, iterations = 0, iter_max = 1000, min_tolerance = 5):
-    """ Carry out optimization to find position of minimum value for 'f'
-        interval = (a, b, d) is the interval to search within
-        finterval = (f(a), f(b), f(d)) is the function value for each interval
-        f(x) is the function to be minimized
-    """
-    a = interval[0]
-    b = interval[1]
-    d = interval[2]
-    
-    f1 = finterval[0]
-    f2 = finterval[1]
-    f4 = finterval[2]
-    
-    if (tolerance > min_tolerance or iter_max < iterations):
-        
-        # Evaluate values
-        c = golden_section(b, d)
-        f3 = f(c)
-
-        print('iter: %d' % iterations, 'b: %f' % b)
-        
-        if f2 < f3:
-            d = c
-            f4 = f3
-            tolerance = abs(f3 - f1)
-            golden_section_interval_reduction((a, b, d), f, (f1, f2, f4), tolerance, iterations+1, iter_max, min_tolerance)
-        
-        elif f2 > f3 :
-            a = b
-            b = c
-            f1 = f2
-            f2 = f3
-            tolerance = abs(f2 - f4)
-            golden_section_interval_reduction((a, b, d), f, (f1, f2, f4), tolerance, iterations+1, iter_max, min_tolerance)
-            
-        elif f2 == f3:
-            d = c
-            f4 = f3
-            tolerance = abs(f3 - f1)
-            golden_section_interval_reduction((a, b, d), f, (f1, f2, f4), tolerance, iterations+1, iter_max, min_tolerance)
-
-    return b
-    
-def test_function(x):
-    return (x-10)**2
-
-def gradient_descent(z_initial, f, tolerance = 50, max_iter = 100000, alpha = 0.1):
-    """ Carry out steepest descent to find minimum of a unimodal R^1 function 
-    gradient_descent(z_initial, f , tolerance = 50, max_iter = 100000, alpha = 0.1)
-    z_initial    : inital search postion
-    f            : function to find minimum of 
-    tolerance    : difference of function evaluations in each iteration
-    max_iter     : maximum no: of iterations
-    alpha        : learning rate
-    """
-       
-    # Initial calculations
-    z = z_initial + alpha
-    f_current = f(z_initial)
-    f_next = f(z)
-    gradient = (f_next - f_current)/alpha
-    converged = False
-    iterations = 1
-    
-    # Steepest descent
-    while (not converged and max_iter > iterations):
-    
-        z = z - alpha * gradient    # update search position
-        f_previous = f_current      # update search history
-        f_nearby = f(z+alpha)       # check nearby position to evaluate greadient
-        f_current = f(z)            # update current search value
-        gradient = (f_nearby - f_current)/alpha     # calculate approx. gradient 
-        iterations += 1             # update no of iterations
-        
-        if abs(f_current-f_previous)<tolerance:
-            print ('Reached minimum tolerance')
-            converged = True
-        elif iterations>max_iter:
-            print('Reached max no: of iterations')
-     
-    # Return results
-    print('Minimum at z = %f' % z)
-    return z
-            
-    
-def z_position(z_current, z_min, z_max):
-    """ Intepret z_position requests as motor steps"""
-    pass
-
-    
    
-# if __name__ == '__main__':
-
-    # X = cv2.imread('C:\\Users\\RAK\\Documents\\iGEM\\Software\\lighthouse.jpg', cv2.IMREAD_GRAYSCALE)
-    # print('Image read is of shape ', X.shape)
-    # #Y = dwt(X)
-    # Ys = nleveldwt(3, X)
-    # print('Finished DWT decomposition of image')
-    # print('Output image size is ', Ys[0].shape)
-    # i = 0
-    # # for Y in Ys:
-        # # cv2.imwrite('Ys%d.png' % i, Y)
-        # # i += 1
-    # print (focus_score(Ys[1:]))
-    
 if __name__ == '__main__':
-    a = -5
-    d = 10
-    b = golden_section(a,d)
-  
-    #x = golden_section_interval_reduction((a, b , d), test_function, (test_function(a), test_function(b), test_function(d)), min_tolerance = 0.5)
-    
-    x = gradient_descent (5, test_function, tolerance = 0.001, alpha = 0.1)
-        
-# htttps://172.29.9.20:9000/_webshell/control/motor/2/50
-    
+
+    X = cv2.imread('C:\\Users\\RAK\\Documents\\iGEM\\Software\\lighthouse.jpg', cv2.IMREAD_GRAYSCALE)
+    print('Image read is of shape ', X.shape)
+    #Y = dwt(X)
+    Ys = nleveldwt(3, X)
+    print('Finished DWT decomposition of image')
+    #print('Output image size is ', Ys[0].shape)
+    # Save images
+    #i = 0
+    # for Y in Ys:
+        # cv2.imwrite('Ys%d.png' % i, Y)
+        # i += 1
+    print ('Focus score: %f' % focus_score(Ys[1:]))
