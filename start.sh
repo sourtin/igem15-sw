d90513d82e80df78a1f746cf63d0c94407251477
moosd <souramoo@gmx.com>
2015-09-16 23:36:32 +0000

    MicroMaps also works as an image viewer; nice webshell changes

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@
 #source pyenv/bin/activate
 gunicorn -w 1 -b 127.0.0.1:9001 gui.webshell.__main__:app &
 gunicorn -w 4 -b 127.0.0.1:9003 gui.admin.__main__:app &
-gunicorn -w 12 -b 127.0.0.1:9004 gui.map_proxy.__main__:app &
-gunicorn -w 1 -b 127.0.0.1:9005 gui.maps.__main__:app &
+gunicorn -w 1 -b 127.0.0.1:9004 gui.maps.__main__:app &
 nginx/nginx -p nginx -c nginx.conf
