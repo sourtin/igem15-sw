f1cd81f46de0f3e08382e76d48aa1f5c4cab49fe
sourtin <william.earley@gmail.com>
2015-09-16 04:47:38 +0100

    Making some μmaps progress, added some webshell helper functions

--- gui/μap/maps.py
+++ gui/μap/maps.py
@@ -3,9 +3,39 @@ import requests
 import numpy as np
 import cv2
 
+limits = {
+    'caps': 200 * 1048576,
+    'stitching': 32 * 1048576
+}
+
 class MicroMaps:
     def __init__(self):
-        pass
+        self.pos = None
+        self.caps = []
+
+    def prune(self):
+        if self.pos is None:
+            return
+        def score(cap):
+            (xc, yc), _ = cap
+            (xr, yr) = self.pos
+            return (xc-xr)**2 + (yc-yr)**2
+        self.caps.sort(key=score)
+        while sum(im.nbytes for _,im in self.caps) > limits['caps']:
+            self.caps.pop()
+
+    def capture(self, x=None, y=None):
+        def _raw():
+            return np.full((2048,1536,3), 127)
+
+        if self.pos is None:
+            raise Exception
+
+
+    def match(self, x, y, w, h):
+        if self.pos is None:
+            return self.capture()
+            self.pos = x+w//2, y+h//2
 
     def get_test(self, x, y, w, h):
         import hashlib
