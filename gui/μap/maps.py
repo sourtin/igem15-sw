4fcbbc5c611d9cb2eaaa2a007312286713151d62
sourtin <william.earley@gmail.com>
2015-09-16 07:02:48 +0100

    Filled in more of μmaps; some motor problems so can't fully test :(

--- gui/μap/maps.py
+++ gui/μap/maps.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
+from lib.vector import Vector
+from lib.stitch import ReferenceStitcher, StitchContext
 import requests
 import requests.auth
 import numpy as np
 import cv2
+import time
 
 limits = {
     'caps': 200 * 1048576,
     'stitching': 32 * 1048576
 }
+
+factors = {
+    'x': 530/800,
+    'y': 400/600,
+    'centre': Vector(1024,768)
+}
+
+context = StitchContext(limits['stitching'])
+context.features.hsv(1).histeq()
 
 class MicroMaps:
     def __init__(self):
@@ -37,23 +49,38 @@ class MicroMaps:
             return cv2.imdecode(data, cv2.IMREAD_COLOR)
 
         def _motor(axis, amount):
+            print(axis, amount)
+            return
             _request("/_webshell/control/motor/%d/%d" % (axis, amount))
 
         if self.pos is None:
             im = _raw()
             h, w = im.shape[:2]
-            self.pos = x-128, y-128 # 128 is a magic number hack
-                                    # for consistency in the algorithm
-            cap = (x-w//2,y-h//2), im
+            self.pos = x-w//2, y-h//2
+            cap = self.pos, im
             self.caps.append(cap)
             return cap
 
         else:
-            # plot a trajectory and follow it
-            # align as we go to fix for real world
-            raise Exception
+            x0, y0 = self.pos
+            δx = (x - x0) * factors['x']
+            δy = (y - y0) * factors['y']
+            _motor(0, δx)
+            _motor(1, δy)
+            return None
 
-    def match(self, x, y, w, h):
+            # wait for completion!?
+            opos = self.pos
+            self.pos = x, y
+            self.prune()
+
+            im = _raw()
+            for pos, ref in self.caps:
+                cand = ReferenceStitcher(context, ref, pos).align(im)
+                self.pos = opos if cand is None else cand[0]
+                return cand
+
+    def match(self, x, y, w, h, timeout=15):
         def rect_in_rect(cap):
             (xc1, yc1), im = cap
             hc, wc = im.shape[:2]
@@ -66,7 +93,22 @@ class MicroMaps:
 
         if self.pos is None:
             self.capture(x, y)
-        return [cap for cap in self.caps if rect_in_rect(cap)]
+        matches = [cap for cap in self.caps if rect_in_rect(cap)]
+        start = time.time()
+        while not len(matches) and (time.time() - start) < timeout:
+            # calculate suitable trajectory to target
+            ori = Vector(*self.pos) - factors['centre']
+            dst = Vector(x, y)
+            vec = dst - ori
+            δ = vec.normalise() * 768
+            xx, yy = ori + (vec if vec.r() < δ.r() else δ)
+
+            new = self.capture(xx, yy)
+            if new is None:
+                break
+            elif rect_in_rect(new):
+                matches.append(new)
+        return matches
 
     def get_test(self, x, y, w, h):
         import hashlib
